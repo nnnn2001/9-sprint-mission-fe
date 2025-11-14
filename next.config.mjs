201a8615dcc794679fa193e24b74850017cb372a
nnnn2001 <hee2ees2@gmail.com>
2025-11-14 13:13:47 +0900

    feat: 이미지 remotePatterns 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
