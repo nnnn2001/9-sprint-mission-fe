7de9769139df0a101e43f0d61879ef05cf89cf7a
nnnn2001 <hee2ees2@gmail.com>
2025-11-14 13:14:17 +0900

    feat: Header Link 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className="w-full flex sm:flex-row md:flex-row justify-between max-w-6xl mx-auto px-4 py-2 items-center ">
       <div className=" py-4 flex sm:flex-row md:flex-row items-center gap-10">
         <Link href="/">
           <div
-            className={`text-2xl font-black flex sm:flex-row md:flex-row items-center justify-center gap-3 text-[#3692FF] font-[family-name:var(--font-rokaf-sans)]`}
+            className={`text-2xl font-bold flex sm:flex-row md:flex-row items-center justify-center gap-3 text-[#3692FF] font-[family-name:var(--font-rokaf-sans)]`}
           >
             <Image src="/panda.png" alt="panda" width={40} height={40} />
             판다마켓
@@ -15,15 +19,32 @@ export default function Header() {
         </Link>
         <nav>
           <ul className="flex space-x-6 font-bold">
-            <li className="text-[#3692FF] font-black">자유게시판</li>
-            <li>중고마켓</li>
+            <Link
+              href="/"
+              className={`${
+                pathname === "/" ? "text-[#3692FF] font-bold" : "text-gray-500"
+              }`}
+            >
+              <li>자유게시판</li>
+            </Link>
+            <Link
+              href={`/product/1`}
+              className={`${
+                pathname === "/product/1"
+                  ? "text-[#3692FF] font-bold"
+                  : "text-gray-500"
+              }`}
+            >
+              <li>중고마켓</li>
+            </Link>
           </ul>
         </nav>
       </div>
-
-      <div className="bg-[#3692FF] w-23 h-10 p-2 rounded-lg text-white font-bold flex justify-center items-center">
-        로그인
-      </div>
+      <Link href="/login">
+        <div className="bg-[#3692FF] w-23 h-10 p-2 rounded-lg text-white font-bold flex justify-center items-center">
+          로그인
+        </div>
+      </Link>
     </header>
   );
 }
